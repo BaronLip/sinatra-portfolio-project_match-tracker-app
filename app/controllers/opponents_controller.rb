ce243645dc1eb8a34b7413acfe150ed932be3369
BaronLip <baronlip@gmail.com>
2019-09-11 20:54:33 -0500

    Updated adding opponent logic.
    Does not allow for duplicate opponent username.

--- app/controllers/opponents_controller.rb
+++ app/controllers/opponents_controller.rb
@@ -6,16 +6,14 @@ class OpponentsController < ApplicationController
 
     post '/opponents/new' do
         @opponent = Opponent.find_by(:username => "New Opponent")
-        @opponent.update(params[:opponent])
-        redirect '/matches/new'
-        # The below does not work because the Opponent is already "saved". How else could we validate the opponent?
-        # binding.pry
-        # if @opponent.save
-        #     redirect '/matches/new'
-        # else
-        #     flash[:errors] = @opponent.errors.full_messages
-        #     erb :'/opponents/new'
-        # end
+        binding.pry
+        if @opponent.update(:username => params[:opponent][:username])
+            @opponent.update(params[:opponent])
+            redirect '/matches/new'
+        else
+            flash[:errors] = @opponent.errors.full_messages.join
+            erb :'/opponents/new'
+        end
     end
 
 end
